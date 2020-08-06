f32088e9d9305975f52e5d13a7fb3f0ec36c2487
CHINMAY <chinmaysri25@gmail.com>
2020-08-06 19:11:13 +0530

    Updates

---
+++ precache-manifest.9b5d6369d3d27aecd7f4b86b472dab45.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4702604f436171f385db0187bae3f8c",
+    "url": "/tgp-library2/index.html"
+  },
+  {
+    "revision": "d76ec1bcedd3b9f13098",
+    "url": "/tgp-library2/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "61e2ebd18a171123d684",
+    "url": "/tgp-library2/static/css/main.45435c08.chunk.css"
+  },
+  {
+    "revision": "d76ec1bcedd3b9f13098",
+    "url": "/tgp-library2/static/js/2.60e448e3.chunk.js"
+  },
+  {
+    "revision": "e8c616cf5dfcba4a1ce47da83898e92f",
+    "url": "/tgp-library2/static/js/2.60e448e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61e2ebd18a171123d684",
+    "url": "/tgp-library2/static/js/main.5a7dbe7b.chunk.js"
+  },
+  {
+    "revision": "a3ca12400ebaac3106fb",
+    "url": "/tgp-library2/static/js/runtime-main.e80de003.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tgp-library2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e751f2698d5c9462190b716bfebc64b7",
+    "url": "/tgp-library2/static/media/placehold.e751f269.jpeg"
+  }
+]);
